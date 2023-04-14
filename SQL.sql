c256ecea294d7fd96469370e33c8f6f68c37b5bc
lkuca <120181604+lkuca@users.noreply.github.com>
2023-04-14 12:58:25 +0300

    Update and rename SQL.txt to SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,37 @@
+SELECT * FROM table_name
+----------------------->
+SELECT - выбрать
+* - все
+FROM - из / откуда
+table_name - имя таблицы
+------------------------<
+CREATE TABLE table_name(column1 dataType,...)
+------------------------------------->
+CREATE -создать
+TABLE таблица
+tabel_name(column1 dataType) - имя таблицы(первый столбец тип данных, и так далее)
+-------------------------------------------<
+INSERT INTO table_name (column_name1, ...)
+VALUES (column_value1, ...)
+----------------------------------------->
+INSERT -добавить 
+INTO - в
+table_name (column_name1, ...) - название таблицы (название столбца, и так далее)
+VAlUES (column_value1, ...) -значения (значение_столбца, и так далее)
+-----------------------------------------<
+DROP TABLE table_name
+--------------------------------->
+DROP - сбросить
+TABLE - таблица
+table_name - имя таблицы
+------------------------<
+UPDATE table_name SET column_name1=column_value1
+WHERE condition
+------------------------------------------------->
+UPDATE - обновить
+table_name
+SET
+column_name1=column_value1
+WHERE
+condition
+
