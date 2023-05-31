3728a421fd9b1833eef46465408e0213c4197754
MaksimDotskin <124581027+MaksimDotskin@users.noreply.github.com>
2023-05-31 11:29:06 +0300

    Update innerJoin.sql

--- innerJoin.sql
+++ innerJoin.sql
@@ -12,6 +12,7 @@ select t.eesnimi,t.perenimi
 from tootaja t
 right join amet a on t.ametID=a.ametID;
 
+--Tabeli loomine
 create table Kursus(
 kursusID int not null primary key,
 kursuseNimetus varchar(20),
@@ -21,3 +22,23 @@ kestvus int);
 alter table Kursus
 add foreign key (tootajaID)
 references tootaja(tootajaID);
+
+--Select taotlused
+
+select t.eesnimi,a.amet,k.kursuseNimetus
+from tootaja as t
+join amet as a on t.ametID=a.ametID
+join Kursus as k on t.tootajaID=k.tootajaID
+where a.ametID=3;
+
+select t.eesnimi,a.amet,k.kursuseNimetus
+from tootaja as t
+join amet as a on t.ametID=a.ametID
+join Kursus as k on t.tootajaID=k.tootajaID
+where  a.amet LIKE 'p%';
+
+select t.eesnimi,a.amet,k.kursuseNimetus
+from tootaja as t
+join amet as a on t.ametID=a.ametID
+join Kursus as k on t.tootajaID=k.tootajaID
+where k.kestvus>3 and k.kursuseNimetus like 's%';
